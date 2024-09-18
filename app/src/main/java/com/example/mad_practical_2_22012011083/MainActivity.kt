32288399a0c4e99859b091b551b5db1c8e220153
naishil19 <naishilpatel19@gmail.com>
2024-09-18 09:55:29 +0530

    All Done

--- app/src/main/java/com/example/mad_practical_2_22012011083/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_2_22012011083/MainActivity.kt
@@ -1,20 +1,68 @@
 package com.example.mad_practical_2_22012011083
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Log.d(TAG, "onCreate called")
+        showToast("onCreate called")
+        //showSnackbar("onCreate called")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+        showToast("onStart called")
+        //showSnackbar("onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+        showToast("onResume called")
+        //showSnackbar("onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+        showToast("onPause called")
+        //showSnackbar("onPause called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart called")
+        showToast("onRestart called")
+        //showSnackbar("onRestart called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+        showToast("onStop called")
+        //showSnackbar("onStop called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+        showToast("onDestroy called")
+        //showSnackbar("onDestroy called")
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+//    private fun showSnackbar(message: String) {
+//        Snackbar.make(findViewById(R.id.main), message, Snackbar.LENGTH_SHORT).show()
+//    }
 }
